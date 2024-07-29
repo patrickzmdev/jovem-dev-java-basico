2db232ca0652433ad87fb606abac6860a53991c2
Patrick Zanela Medeiros <patrickzm290493@gmail.com>
2024-07-29 17:25:18 -0300

    update

--- src/aula1/collections/Exercicio4.java
+++ src/aula1/collections/Exercicio4.java
@@ -1,8 +1,7 @@
 package aula1.collections;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+
 import java.util.Map;
 
 import javax.swing.JOptionPane;
@@ -10,17 +9,18 @@ import javax.swing.JOptionPane;
 public class Exercicio4 {
 
 	public static void main(String[] args) {
-		
+
 		Map<Integer, String> mapa = new HashMap<>();
 		String frase = JOptionPane.showInputDialog("Digite uma frase: ");
 		
 		for (int i = 0; i < frase.length(); i++) {
-			mapa.put(null, frase);
+			char letras = frase.charAt(i);
+			String teste = Character.toString(letras);
+			mapa.put(i, teste );
 		}
-		
+
 		System.out.println(mapa);
 
 	}
-
 
 }
